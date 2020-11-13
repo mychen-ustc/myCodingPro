ecaed4796810ba50f98d373b7c073a52c9697258
alstonchen <alstonchen@tencent.com>
2020-11-13 20:14:56 +0800

    update

--- src/main/java/_04_recursion_recall/_0051_solveNQueens.java
+++ src/main/java/_04_recursion_recall/_0051_solveNQueens.java
@@ -1,4 +1,7 @@
 /**
+ * 51：n皇后
+ * 难度：困难
+ * <p>
  * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
  * <p>
  * <p>
@@ -39,16 +42,85 @@
 
 package _04_recursion_recall;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution0051 {
     public List<List<String>> solveNQueens(int n) {
+        // 初始化棋盘
+        char[][] chess = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chess[i][j] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        solve(res, chess, 0);
+        return res;
+    }
 
+    /**
+     * 求解
+     *
+     * @param res   结果集
+     * @param chess 棋盘
+     * @param row   行
+     */
+    private void solve(List<List<String>> res, char[][] chess, int row) {
+        if (row == chess.length) {
+            res.add(construct(chess));
+            return;
+        }
+        for (int col = 0; col < chess.length; col++) {
+            if (valid(chess, row, col)) {
+                chess[row][col] = 'Q';  // 在当前位置放一个Q
+                solve(res, chess, row + 1);
+                chess[row][col] = '.';  // 去掉当前位置的Q，继续搜索下一列
+            }
+        }
+    }
+
+    /**
+     * 判断当前位置是否有效
+     *
+     * @param chess 棋盘
+     * @param row   行
+     * @param col   列
+     * @return
+     */
+    private boolean valid(char[][] chess, int row, int col) {
+        // 由于是一行一行往下走，不用检查当前行，只需要检查当前列、右上角、左上角
+        // 判断当前列有没有Q
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // 判断当前坐标右上角有没有Q
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // 判断左上角有没有Q
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<String> construct(char[][] chess) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < chess.length; i++) {
+            path.add(new String(chess[i]));
+        }
+        return path;
     }
 }
 
 public class _0051_solveNQueens {
-
 
     public static void main(String[] args) {
         Solution0051 solution = new Solution0051();

29bbed5e63342870507a577f87c882e6c0e45282
alstonchen <alstonchen@tencent.com>
2020-11-22 21:40:48 +0800

    update

---
+++ src/main/java/_236_lowestCommonAncestor.java
@@ -0,0 +1,77 @@
+/**
+ * 236. 二叉树的最近公共祖先
+ * 难度：中等
+//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+//
+//百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+//
+//例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+//
+//
+//
+// 
+//
+//示例 1:
+//
+//输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出: 3
+//解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+//示例 2:
+//
+//输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出: 5
+//解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+// 
+//
+//说明:
+//
+//所有节点的值都是唯一的。
+//p、q 为不同节点且均存在于给定的二叉树中。
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+    }
+}
+
+public class _236_lowestCommonAncestor {
+
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(5);
+        TreeNode c = new TreeNode(1);
+        TreeNode d = new TreeNode(6);
+        TreeNode e = new TreeNode(2);
+        TreeNode f = new TreeNode(0);
+        TreeNode g = new TreeNode(8);
+        TreeNode h = new TreeNode(7);
+        TreeNode i = new TreeNode(4);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.left = f;
+        c.right = g;
+        e.left = h;
+        e.right = i;
+        Solution_236 solution = new Solution_236();
+        TreeNode node = solution.lowestCommonAncestor(a, b, c);
+        System.out.println(node);
+    }
+
+
+}

6618af5da4fd8bf1bf6eec714157eb42efb2550e
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-11-23 09:02:30 +0800

    update

--- src/main/java/com/code/_05_tree_graph/TreeNode.java
+++ src/main/java/com/code/_05_tree_graph/TreeNode.java
@@ -1,11 +1,32 @@
 package com.code._05_tree_graph;
 
+/* 精简版的定义 */
+//public class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//
+//    TreeNode(int x) {
+//        val = x;
+//    }
+//}
+
+/*提供两个构造器*/
 public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }

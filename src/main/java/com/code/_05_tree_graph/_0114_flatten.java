9a0d9815707063449e6fbb0b18945034d19c8437
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-11-24 09:31:21 +0800

    update

--- src/main/java/com/code/_05_tree_graph/_0114_flatten.java
+++ src/main/java/com/code/_05_tree_graph/_0114_flatten.java
@@ -1,34 +1,34 @@
 /**
  * 114. 二叉树展开为链表
  * 难度：中等
-// * 给定一个二叉树，原地将它展开为一个单链表。
-// *
-// *  
-// *
-// * 例如，给定二叉树
-// *
-// *     1
-// *    / \
-// *   2   5
-// *  / \   \
-// * 3   4   6
-// * 将其展开为：
-// *
-// * 1
-// *  \
-// *   2
-// *    \
-// *     3
-// *      \
-// *       4
-// *        \
-// *         5
-// *          \
-// *           6
-// *
-// * 来源：力扣（LeetCode）
-// * 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
-// * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * // 给定一个二叉树，原地将它展开为一个单链表。
+ * //
+ * //  
+ * //
+ * // 例如，给定二叉树
+ * //
+ * //     1
+ * //    / \
+ * //   2   5
+ * //  / \   \
+ * // 3   4   6
+ * // 将其展开为：
+ * //
+ * // 1
+ * //  \
+ * //   2
+ * //    \
+ * //     3
+ * //      \
+ * //       4
+ * //        \
+ * //         5
+ * //          \
+ * //           6
+ * //
+ * // 来源：力扣（LeetCode）
+ * // 链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list
+ * // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
 package com.code._05_tree_graph;
@@ -36,16 +36,16 @@ package com.code._05_tree_graph;
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 
@@ -57,7 +57,7 @@ class Solution_0114 {
 
     public TreeNode preorder(TreeNode node) {
         TreeNode last = null;
-        if(node == null)
+        if (node == null)
             return null;
         if (node.left == null && node.right == null) {
             last = node;

bd30d432b4b5d2688520af5d50074986af69dcbf
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-11-25 22:10:45 +0800

    add 200 bfs

---
+++ src/main/java/com/code/_8_search/_0200_numIslands_bfs.java
@@ -0,0 +1,101 @@
+/**
+ * // 200. 岛屿数量
+ * // 难度：中等
+ * //
+ * // 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * //
+ * // 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * //
+ * // 此外，你可以假设该网格的四条边均被水包围。
+ * //
+ * //  
+ * //
+ * // 示例 1：
+ * //
+ * // 输入：grid = [
+ * // ['1','1','1','1','0'],
+ * // ['1','1','0','1','0'],
+ * // ['1','1','0','0','0'],
+ * // ['0','0','0','0','0']
+ * // ]
+ * // 输出：1
+ * // 示例 2：
+ * //
+ * // 输入：grid = [
+ * // ['1','1','0','0','0'],
+ * // ['1','1','0','0','0'],
+ * // ['0','0','1','0','0'],
+ * // ['0','0','0','1','1']
+ * // ]
+ * // 输出：3
+ * //  
+ * //
+ * // 提示：
+ * //
+ * // m == grid.length
+ * // n == grid[i].length
+ * // 1 <= m, n <= 300
+ * // grid[i][j] 的值为 '0' 或 '1'
+ * //
+ * // 来源：力扣（LeetCode）
+ * // 链接：https://leetcode-cn.com/problems/number-of-islands
+ * // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+package com.code._8_search;
+
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution_0200_bfs {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0)
+            return 0;
+        int numIsland = 0;
+        int[][] mark = new int[grid.length][grid[0].length];  // 定义标记数组，用于标记位置是否已经被搜索
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (mark[i][j] == 0 && grid[i][j] == '1') { // 如果位置没被搜索，且为陆地，则向四周搜索
+                    bfs(grid, mark, i, j);
+                    numIsland++;
+                }
+            }
+        }
+        return numIsland;
+    }
+
+    public void bfs(char[][] grid, int[][] mark, int x, int y) {
+        final int[] DX = {-1, 1, 0, 0};     // 方向数组的X坐标偏移量
+        final int[] DY = {0, 0, -1, 1};     // 方向数组的Y坐标偏移量
+        mark[x][y] = 1;     // 标记当前位置
+        Queue<Pair<Integer, Integer>> queue = new LinkedList<>();
+        queue.offer(new Pair<>(x, y));  // 将当前节点加入
+        while (!queue.isEmpty()) {
+            Pair<Integer, Integer> pair = queue.poll();   // 取队头
+            x = pair.getKey();
+            y = pair.getValue();
+            for (int i = 0; i < 4; i++) {   // 对队头节点向4个方向搜索
+                int newx = DX[i] + x;   // 新的x坐标
+                int newy = DY[i] + y;   // 新的y坐标
+                if (newx < 0 || newy < 0 || newx >= mark.length || newy >= mark[newx].length) {
+                    continue;   // 越过边界
+                }
+                if (mark[newx][newy] == 0 && grid[newx][newy] == '1') {
+                    queue.offer(new Pair<>(newx, newy));    // 将新的位置push到队列
+                    bfs(grid, mark, newx, newy);
+                }
+            }
+        }
+    }
+}
+
+public class _0200_numIslands_bfs {
+    public static void main(String[] args) {
+        char[][] grid = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'}};
+        Solution_0200_bfs solution = new Solution_0200_bfs();
+        int result = solution.numIslands(grid);
+        System.out.println(result);
+    }
+}

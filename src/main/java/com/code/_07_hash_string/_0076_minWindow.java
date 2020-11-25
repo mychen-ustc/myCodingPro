82204a40153f3f96c699bbb86bdc521feb7b8ace
alstonchen <alstonchen@tencent.com>
2020-11-25 17:13:29 +0800

    add 76

--- src/main/java/com/code/_07_hash_string/_0076_minWindow.java
+++ src/main/java/com/code/_07_hash_string/_0076_minWindow.java
@@ -33,9 +33,61 @@
 
 package com.code._07_hash_string;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution_0076 {
     public String minWindow(String s, String t) {
+        int[] mapSource = new int[128]; // 源字符串的字符个数映射，默认都初始化为0
+        int[] mapTarget = new int[128]; // 目标字符串的字符个数映射，默认都初始化为0
+        Set<Character> chars = new HashSet<>();  // 目标字符串中包含的所有字符，用于快速索引
+        String result = ""; // 存储结果字符串
+        int windowBegin = 0;    // 字符串串口的起始位置
+        // 遍历目标字符串，并做响应的计数
+        for (int i = 0; i < t.length(); i++) {
+            chars.add(t.charAt(i));
+            mapTarget[t.charAt(i)]++;
+        }
+        // 遍历数据，移动字符串窗口进行统计
+        for (int i = 0; i < s.length(); i++) {
+            mapSource[s.charAt(i)]++;
+            while (windowBegin < i) {
+                char ch = s.charAt(windowBegin);
+                if (mapTarget[ch] == 0) {     // 如果首字符在目标字符串中不存在，可将头指针后移一位
+                    windowBegin++;  // 窗口头指针后移1位
+                } else if (mapSource[ch] > mapTarget[ch]) {  // 首字符在目标字符串中存在，但是字符的个数多余目标字符串，可以后移
+                    windowBegin++;  // 窗口头指针后移一位
+                    mapSource[ch]--;    // 头指针后移1位，需要把对应的字符个数减1
+                } else {
+                    break;
+                }
+            }
+            // 处理结果字符串
+            if (isCovered(mapSource, mapTarget, chars)) {
+                int windowLen = i - windowBegin + 1;
+                if (result.length() == 0 || result.length() > windowLen) {
+                    result = s.substring(windowBegin, i + 1);
+                }
+            }
+        }
+        return result;
+    }
 
+    /**
+     * 判断 mapSource 是否完整包含 mapTarget 的所有字符
+     *
+     * @param mapSource 源字符串的字符映射
+     * @param mapTarget 目标字符串的字符映射
+     * @param chars     目标字符串包含的所有字符
+     * @return
+     */
+    public Boolean isCovered(int[] mapSource, int[] mapTarget, Set<Character> chars) {
+        for (char ch : chars) {
+            if (mapSource[ch] < mapTarget[ch]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 

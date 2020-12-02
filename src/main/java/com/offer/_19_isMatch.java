7664d9db3ff1c08ab626a188677e5c4aa54f1392
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-03 07:44:53 +0800

    update

--- src/main/java/com/offer/_19_isMatch.java
+++ src/main/java/com/offer/_19_isMatch.java
@@ -45,9 +45,9 @@
 package com.offer;
 
 class Solution_19 {
-    public boolean isMatch(String A, String B) {
-        int n = A.length();
-        int m = B.length();
+    public boolean isMatch(String s, String p) {
+        int n = s.length();
+        int m = p.length();
         boolean[][] dp = new boolean[n + 1][m + 1];
 
         for (int i = 0; i <= n; i++) {
@@ -57,8 +57,8 @@ class Solution_19 {
                     dp[i][j] = i == 0;
                 } else {
                     //非空正则分为两种情况 * 和 非*
-                    if (B.charAt(j - 1) != '*') {
-                        if (i > 0 && (A.charAt(i - 1) == B.charAt(j - 1) || B.charAt(j - 1) == '.')) {
+                    if (p.charAt(j - 1) != '*') {
+                        if (i > 0 && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.')) {
                             dp[i][j] = dp[i - 1][j - 1];
                         }
                     } else {
@@ -68,7 +68,7 @@ class Solution_19 {
                             dp[i][j] |= dp[i][j - 2];
                         }
                         //看
-                        if (i >= 1 && j >= 2 && (A.charAt(i - 1) == B.charAt(j - 2) || B.charAt(j - 2) == '.')) {
+                        if (i >= 1 && j >= 2 && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.')) {
                             dp[i][j] |= dp[i - 1][j];
                         }
                     }

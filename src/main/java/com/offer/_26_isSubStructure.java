d4224c436a44165d0eae5a0a67b1560abfb2ec89
alstonchen <alstonchen@tencent.com>
2020-12-03 12:49:31 +0800

    add offer 26

--- src/main/java/com/offer/_26_isSubStructure.java
+++ src/main/java/com/offer/_26_isSubStructure.java
@@ -45,12 +45,40 @@ package com.offer;
  */
 class Solution_26 {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null)     // 如果某棵树为空，返回false
+            return false;
 
+        return judge(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    // 从当前节点开始判断是否满足条件
+    boolean judge(TreeNode A, TreeNode B) {
+        if (B == null)  // B节点已经遍历完，返回true
+            return true;
+        if (A == null || A.val != B.val)    // 如果A节点都比较完了，或者当前根节点值不同，返回false
+            return false;
+        return judge(A.left, B.left) && judge(A.right, B.right);    // 判断左右子树是否满足条件
     }
 }
 
 public class _26_isSubStructure {
     public static void main(String[] args) {
+        TreeNode a1 = new TreeNode(3);
+        TreeNode b1 = new TreeNode(4);
+        TreeNode c1 = new TreeNode(5);
+        TreeNode d1 = new TreeNode(1);
+        TreeNode e1 = new TreeNode(2);
+        a1.left = b1;
+        a1.right = c1;
+        b1.left = d1;
+        b1.right = e1;
 
+        TreeNode a2 = new TreeNode(4);
+        TreeNode b2 = new TreeNode(1);
+        a2.left = b2;
+
+        Solution_26 solution = new Solution_26();
+        boolean ans = solution.isSubStructure(a1, a2);
+        System.out.println(ans);
     }
 }

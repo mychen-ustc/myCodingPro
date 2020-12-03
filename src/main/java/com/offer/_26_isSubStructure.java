65f1671de5ad5aad3549239b8bd38cc78ee0cda6
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-03 09:31:37 +0800

    update

---
+++ src/main/java/com/offer/_26_isSubStructure.java
@@ -0,0 +1,56 @@
+/**
+ * // 26. 树的子结构
+ * // 难度：中等
+ * // 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ * //
+ * // B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ * //
+ * // 例如:
+ * // 给定的树 A:
+ * //
+ * //      3
+ * //     / \
+ * //    4   5
+ * //   / \
+ * //  1   2
+ * // 给定的树 B：
+ * //
+ * //    4 
+ * //   /
+ * //  1
+ * // 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ * //
+ * // 示例 1：
+ * // 输入：A = [1,2,3], B = [3,1]
+ * // 输出：false
+ * //
+ * // 示例 2：
+ * // 输入：A = [3,4,5,1,2], B = [4,1]
+ * // 输出：true
+ * //
+ * // 限制：
+ * // 0 <= 节点个数 <= 10000
+ */
+
+package com.offer;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_26 {
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+
+    }
+}
+
+public class _26_isSubStructure {
+    public static void main(String[] args) {
+
+    }
+}

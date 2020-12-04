98ec302f7de397d116708d7d5385cc9df4c344c1
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-04 21:16:49 +0800

    add 32_II

--- src/main/java/com/offer/_32_II_levelOrder.java
+++ src/main/java/com/offer/_32_II_levelOrder.java
@@ -27,25 +27,65 @@
 
 package com.offer;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
  * }
  */
 class Solution_32_II {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        return null;
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();   // 用pair存储节点和层
+        if (root == null) {
+            return ans;
+        }
+        queue.add(new Pair(root, 0));   // 将根节点添加到队列
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Integer> pair = queue.poll();     // 取队头
+            TreeNode node = pair.getKey();  // 获取节点
+            int depth = pair.getValue();
+            if (ans.size() < depth + 1) {  // 当前层的列表尚未生成，先添加一个列表
+                ans.add(new ArrayList<>());
+            }
+            ans.get(depth).add(node.val);
+
+            if (node.left != null) {
+                queue.add(new Pair(node.left, depth + 1));  // 左子节点入队列
+            }
+            if (node.right != null) {
+                queue.add(new Pair<>(node.right, depth + 1));    // 右子节点入队列
+            }
+        }
+        return ans;
     }
 }
 
 public class _32_II_levelOrder {
     public static void main(String[] args) {
         // [3,9,20,null,null,15,7]
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(9);
+        TreeNode c = new TreeNode(20);
+        TreeNode d = new TreeNode(15);
+        TreeNode e = new TreeNode(7);
+        a.left = b;
+        a.right = c;
+        c.left = d;
+        c.right = e;
+
+        Solution_32_II solution = new Solution_32_II();
+        List<List<Integer>> ans = solution.levelOrder(a);
+        System.out.println(ans);
     }
 }

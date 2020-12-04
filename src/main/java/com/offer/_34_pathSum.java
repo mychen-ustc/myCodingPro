1e0fa1eaa85cfd4a038e483db3f75c4dcb994993
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-04 09:35:56 +0800

    update

---
+++ src/main/java/com/offer/_34_pathSum.java
@@ -0,0 +1,51 @@
+/**
+ * // 34. 二叉树中和为某一值的路径
+ * // 难度：中等
+ * // 输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+ * //
+ * // 示例:
+ * // 给定如下二叉树，以及目标和 sum = 22，
+ * // 5
+ * // / \
+ * // 4   8
+ * // /   / \
+ * // 11  13  4
+ * // /  \    / \
+ * // 7    2  5   1
+ * //
+ * // 返回:
+ * // [
+ * // [5,4,11,2],
+ * // [5,8,4,5]
+ * // ]
+ * //
+ * // 提示：
+ * // 节点总数 <= 10000
+ * // 注意：本题与主站 113 题相同：https://leetcode-cn.com/problems/path-sum-ii/
+ */
+
+package com.offer;
+
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        return null;
+    }
+}
+
+public class _34_pathSum {
+    public static void main(String[] args) {
+        // [5,4,8,11,null,13,4,7,2,null,null,5,1]
+        // 22
+    }
+}

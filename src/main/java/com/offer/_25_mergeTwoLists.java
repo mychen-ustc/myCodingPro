c7686c5c0018f55ba3ecba04f8a22bd0f8cce225
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-04 20:55:10 +0800

    update

--- src/main/java/com/offer/_25_mergeTwoLists.java
+++ src/main/java/com/offer/_25_mergeTwoLists.java
@@ -25,6 +25,12 @@ package com.offer;
  */
 class Solution_25 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+            return null;
+        if (l1 == null && l2 != null)
+            return l2;
+        if (l1 != null && l2 == null)
+            return l1;
         ListNode head;  // 新链表的表头
         ListNode cur;   // 新链表的尾节点
         if (l1.val <= l2.val) {

08e9bf8e86734476fcd1101cbcd6347db33a32e8
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-04 08:46:17 +0800

    update

---
+++ src/main/java/com/offer/_30_MinStack.java
@@ -0,0 +1,65 @@
+/**
+ * // 30. 包含min函数的栈
+ * // 难度：简单
+ * // 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+ * //
+ * // 示例:
+ * //
+ * // MinStack minStack = new MinStack();
+ * // minStack.push(-2);
+ * // minStack.push(0);
+ * // minStack.push(-3);
+ * // minStack.min();   --> 返回 -3.
+ * // minStack.pop();
+ * // minStack.top();      --> 返回 0.
+ * // minStack.min();   --> 返回 -2.
+ * //
+ * // 提示：
+ * // 各函数的调用总次数不超过 20000 次
+ * //
+ * // 注意：本题与主站 155 题相同：https://leetcode-cn.com/problems/min-stack/
+ */
+
+package com.offer;
+
+class MinStack_30 {
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int min() {
+
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.min();
+ */
+
+public class _30_MinStack {
+    public static void main(String[] args) {
+        // ["MinStack","push","push","push","min","pop","top","min"]
+        // [[],[-2],[0],[-3],[],[],[],[]]
+    }
+}
---
+++ src/main/java/com/offer/_32_I_levelOrder.java
@@ -0,0 +1,43 @@
+/**
+ * // 32 - I. 从上到下打印二叉树
+ * // 难度：中等
+ * // 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ * //
+ * // 例如:
+ * // 给定二叉树: [3,9,20,null,null,15,7],
+ * //
+ * // 3
+ * // / \
+ * // 9  20
+ * // /  \
+ * // 15   7
+ * // 返回：
+ * //
+ * // [3,9,20,15,7]
+ * //
+ * // 提示：
+ * // 节点总数 <= 1000
+ */
+
+package com.offer;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_31_I {
+    public int[] levelOrder(TreeNode root) {
+
+    }
+}
+
+public class _32_I_levelOrder {
+    public static void main(String[] args) {
+        // [3,9,20,null,null,15,7]
+    }
+}

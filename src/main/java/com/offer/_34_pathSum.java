74c339b5b6befa7545a2d14ce6277ad9bb2b76fb
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-05 11:00:31 +0800

    add offer 34

--- src/main/java/com/offer/_34_pathSum.java
+++ src/main/java/com/offer/_34_pathSum.java
@@ -5,12 +5,12 @@
  * //
  * // 示例:
  * // 给定如下二叉树，以及目标和 sum = 22，
- * // 5
- * // / \
- * // 4   8
- * // /   / \
- * // 11  13  4
- * // /  \    / \
+ * //      5
+ * //     / \
+ * //    4   8
+ * //   /   / \
+ * //  11  13  4
+ * //  /  \    / \
  * // 7    2  5   1
  * //
  * // 返回:
@@ -26,7 +26,9 @@
 
 package com.offer;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Definition for a binary tree node.
@@ -37,9 +39,34 @@ import java.util.List;
  * TreeNode(int x) { val = x; }
  * }
  */
-class Solution {
+class Solution_34 {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        return null;
+        List<List<Integer>> ans = new ArrayList<>();
+        Stack<Integer> path = new Stack<>();
+        dfs(ans, path, root, sum);
+        return ans;
+    }
+
+    /**
+     * 递归+回溯，深度优先搜索求路径
+     *
+     * @param ans  结果集
+     * @param path 当前处理的路径
+     * @param node 当前节点
+     * @param sum  当前需要处理的目标和
+     */
+    public void dfs(List<List<Integer>> ans, Stack<Integer> path, TreeNode node, int sum) {
+        if (node == null)
+            return;
+        path.push(node.val);    // 将当前节点添加到路径中
+        sum -= node.val;    // 将目标和减去当前节点的值
+        if (node.left == null && node.right == null && sum == 0) {   // 如果当前节点为叶子节点，并且目标和为0，则满足要求
+            ans.add(new ArrayList<>(path));     // 将当前路径添加到结果集
+        }
+        dfs(ans, path, node.left, sum);     // 递归求解子树
+        dfs(ans, path, node.right, sum);    // 递归求解子树
+
+        path.pop();     // 弹出当前节点，进行回溯
     }
 }
 
@@ -47,5 +74,27 @@ public class _34_pathSum {
     public static void main(String[] args) {
         // [5,4,8,11,null,13,4,7,2,null,null,5,1]
         // 22
+        TreeNode a = new TreeNode(5);
+        TreeNode b = new TreeNode(4);
+        TreeNode c = new TreeNode(8);
+        TreeNode d = new TreeNode(11);
+        TreeNode e = new TreeNode(13);
+        TreeNode f = new TreeNode(4);
+        TreeNode g = new TreeNode(7);
+        TreeNode h = new TreeNode(2);
+        TreeNode i = new TreeNode(5);
+        TreeNode j = new TreeNode(1);
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        c.left = e;
+        c.right = f;
+        d.left = g;
+        d.right = h;
+        f.left = i;
+        f.right = j;
+        Solution_34 solution = new Solution_34();
+        List<List<Integer>> res = solution.pathSum(a, 22);
+        System.out.println(res);
     }
 }

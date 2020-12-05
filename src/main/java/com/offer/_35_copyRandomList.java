c73adcbd6c3a9ce432604709983120bc8c555a15
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-05 12:52:10 +0800

    add offer 35

---
+++ src/main/java/com/offer/_35_copyRandomList.java
@@ -0,0 +1,117 @@
+/**
+ * // 35. 复杂链表的复制
+ * // 难度：中等
+ * // 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ * //
+ * // 示意图见 _35_copyRandomList.jpeg
+ * // 示例 1：
+ * // 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * // 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * //
+ * // 示例 2：
+ * // 输入：head = [[1,1],[2,1]]
+ * // 输出：[[1,1],[2,1]]
+ * //
+ * // 示例 3：
+ * // 输入：head = [[3,null],[3,0],[3,null]]
+ * // 输出：[[3,null],[3,0],[3,null]]
+ * //
+ * // 示例 4：
+ * // 输入：head = []
+ * // 输出：[]
+ * // 解释：给定的链表为空（空指针），因此返回 null。
+ * //
+ * // 提示：
+ * // -10000 <= Node.val <= 10000
+ * // Node.random 为空（null）或指向链表中的节点。
+ * // 节点数目不超过 1000 。
+ * //
+ * // 注意：本题与主站 138 题相同：https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+ * //
+ * // 分析：next指针的复制没什么问题，主要是考虑random指针的复制，关键是要记录random指向的是第几个节点
+ */
+
+package com.offer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+class Solution_35 {
+    public Node copyRandomList(Node head) {
+        Node tmp = head;    // 用一个临时节点拷贝head
+        if (head == null) return null;
+        // 定义链表，按顺序存储节点，便于索引节点，给random指针赋值。结点的next指向后一个节点即可
+        List<Node> newNodes = new ArrayList<>();
+        List<Node> oldNodes = new ArrayList<>();
+        // 定义hashmap，存储原始节点的hashcode和节点索引序号
+        Map<Integer, Integer> map = new HashMap<>();
+        // 定义一个数组，记录random的索引，索引的结点在newNodes链表中
+        List<Integer> indexs = new ArrayList<>();
+        // 第一次遍历：创建所有新结点
+        int cnt = 0;
+        while (tmp != null) {
+            Node node = new Node(tmp.val);
+            newNodes.add(node); // 把新节点存储到链表中
+            oldNodes.add(tmp);  // 把旧节点存储到链表中
+            map.put(tmp.hashCode(), cnt);
+            cnt++;
+            tmp = tmp.next;
+        }
+        // 处理新节点的next和random指针
+        for (int i = 0; i < newNodes.size(); i++) {
+            Node node = newNodes.get(i);
+            // 处理next指针
+            if (i < newNodes.size() - 1) {
+                node.next = newNodes.get(i + 1);
+            } else {
+                node.next = null;   // 这个赋值其实可以省略，因为节点next指针已被初始化为null
+            }
+            // 处理random指针
+            Node oldNode = oldNodes.get(i);     // 取旧节点
+            if (oldNode.random != null) {
+                int oldHashCode = oldNode.random.hashCode();
+                int index = map.get(oldHashCode);
+                node.random = newNodes.get(index);
+            }
+        }
+        return newNodes.get(0);     // 返回新的头结点
+    }
+}
+
+public class _35_copyRandomList {
+    public static void main(String[] args) {
+        // [[7,null],[13,0],[11,4],[10,2],[1,0]]
+        Node a = new Node(7);
+        Node b = new Node(13);
+        Node c = new Node(11);
+        Node d = new Node(10);
+        Node e = new Node(1);
+        a.next = b;
+        b.next = c;
+        b.random = a;
+        c.next = d;
+        c.random = e;
+        d.next = e;
+        d.random = c;
+        e.random = a;
+
+        Solution_35 solution = new Solution_35();
+        Node ans = solution.copyRandomList(a);
+        System.out.println(ans);
+    }
+}

425190a471047959d44a9bf7ae5a53da3d8501d4
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-08 23:12:53 +0800

    add offer 53_I

--- src/main/java/com/offer/_53_I_search.java
+++ src/main/java/com/offer/_53_I_search.java
@@ -21,12 +21,56 @@ package com.offer;
 
 class Solution_53_I {
     public int search(int[] nums, int target) {
-        return 0;
+        // 用2个指针，分别找到目标数字第一个和最后一个出现的位置
+        int left = findLeftBound(nums, target);
+        int right = findRightBound(nums, target);
+
+        return (left == -1 && right == -1) ? 0 : (right - left + 1);
+    }
+
+    // 找到第一个等于目标数字的位置
+    int findLeftBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] < target)
+                    return mid;
+                right = mid - 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    // 找到最后一个等于目标数字的位置
+    int findRightBound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] > target)
+                    return mid;
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
     }
 }
 
 public class _53_I_search {
     public static void main(String[] args) {
-
+        int[] nums = {5, 7, 8, 8, 10};
+        int target = 9;
+        Solution_53_I solution = new Solution_53_I();
+        int ans = solution.search(nums, target);
+        System.out.println(ans);
     }
 }

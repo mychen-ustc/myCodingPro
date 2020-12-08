1d4f904cc8fbb275f72a219dccd0f372290faaf5
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-08 08:51:20 +0800

    add offer 52

--- src/main/java/com/offer/_52_getIntersectionNode.java
+++ src/main/java/com/offer/_52_getIntersectionNode.java
@@ -45,7 +45,25 @@ package com.offer;
  */
 class Solution_52 {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return null;
+        // 有三种思路：
+        // 1.用集合记录已访问的节点，如果发现某个节点已经在集合中，则该节点为相交节点，第一个相交节点即为所求。
+        // 2.先统计两个链表的长度，假设差值为diff，然后让长链表的指针先走diff步，之后两个指针同步走，第一个相同的节点即为所求
+        // 3.参照赛跑（先跑完的，从另一个链表头部接着跑），跑的快的和跑的慢的最终一定会在某个点相遇。这里用思路3
+        if (headA == null || headB == null) return null;
+        ListNode pointerA = headA, pointerB = headB;
+        while (pointerA != pointerB) {
+            if (pointerA == null) {
+                pointerA = headB;
+            } else {
+                pointerA = pointerA.next;
+            }
+            if (pointerB == null) {
+                pointerB = headA;
+            } else {
+                pointerB = pointerB.next;
+            }
+        }
+        return pointerA;
     }
 }
 
@@ -56,5 +74,26 @@ public class _52_getIntersectionNode {
         // [5,0,1,8,4,5]
         // 2
         // 3
+        ListNode x = new ListNode(8);
+        ListNode y = new ListNode(4);
+        ListNode z = new ListNode(5);
+        x.next = y;
+        y.next = z;
+
+        ListNode a1 = new ListNode(4);
+        ListNode b1 = new ListNode(1);
+        a1.next = b1;
+        b1.next = x;
+
+        ListNode a2 = new ListNode(5);
+        ListNode b2 = new ListNode(0);
+        ListNode c2 = new ListNode(1);
+        a2.next = b2;
+        b2.next = c2;
+        c2.next = x;
+
+        helper solution = new helper();
+        ListNode ans = solution.getIntersectionNode(a1, a2);
+        System.out.println(ans.val);
     }
 }

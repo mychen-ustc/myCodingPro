872785317a9c3841883c159e6eb29c8ffb9fe6eb
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-08 09:02:37 +0800

    update

---
+++ src/main/java/com/offer/_56_I.java
@@ -0,0 +1,30 @@
+/**
+ * // 56 - I. 数组中数字出现的次数
+ * // 难度：简单
+ * // 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。要求时间复杂度是O(n)，空间复杂度是O(1)。
+ * //
+ * // 示例 1：
+ * // 输入：nums = [4,1,4,6]
+ * // 输出：[1,6] 或 [6,1]
+ * //
+ * // 示例 2：
+ * // 输入：nums = [1,2,10,4,1,4,3,3]
+ * // 输出：[2,10] 或 [10,2]
+ * //
+ * // 限制：
+ * // 2 <= nums.length <= 10000
+ */
+
+package com.offer;
+
+class Solution_56_I {
+    public int[] singleNumbers(int[] nums) {
+        return null;
+    }
+}
+
+public class _56_I {
+    public static void main(String[] args) {
+        // [4,1,4,6]
+    }
+}

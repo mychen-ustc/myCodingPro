b979f931b1ebbf5c4ce96374d354f94b5cb251e7
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-09 08:19:55 +0800

    update

---
+++ src/main/java/com/offer/_59_II_MaxQueue.java
@@ -0,0 +1,59 @@
+/**
+ * // 59 - II. 队列的最大值
+ * // 难度：中等
+ * // 请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
+ * //
+ * // 若队列为空，pop_front 和 max_value 需要返回 -1
+ * //
+ * // 示例 1：
+ * // 输入:
+ * // ["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+ * // [[],[1],[2],[],[],[]]
+ * // 输出: [null,null,null,2,1,2]
+ * //
+ * // 示例 2：
+ * // 输入:
+ * // ["MaxQueue","pop_front","max_value"]
+ * // [[],[],[]]
+ * // 输出: [null,-1,-1]
+ * //
+ * // 限制：
+ * // 1 <= push_back,pop_front,max_value的总操作数 <= 10000
+ * // 1 <= value <= 10^5
+ */
+
+package com.offer;
+
+class MaxQueue {
+
+    public MaxQueue() {
+
+    }
+
+    public int max_value() {
+        return 0;
+    }
+
+    public void push_back(int value) {
+
+    }
+
+    public int pop_front() {
+        return 0;
+    }
+}
+
+/**
+ * Your MaxQueue object will be instantiated and called as such:
+ * MaxQueue obj = new MaxQueue();
+ * int param_1 = obj.max_value();
+ * obj.push_back(value);
+ * int param_3 = obj.pop_front();
+ */
+
+public class _59_II_MaxQueue {
+    public static void main(String[] args) {
+        // ["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+        // [[],[1],[2],[],[],[]]
+    }
+}

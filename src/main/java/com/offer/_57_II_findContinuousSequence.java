0ab193cdaf1a170620346bcdb253ebddd9d21dd4
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-09 21:59:33 +0800

    update

--- src/main/java/com/offer/_57_II_findContinuousSequence.java
+++ src/main/java/com/offer/_57_II_findContinuousSequence.java
@@ -24,24 +24,55 @@ import java.util.List;
 
 class Solution_57_II {
     public int[][] findContinuousSequence(int target) {
-        List<int[]> ans = new ArrayList<>();
-        for (int begin = 1; begin <= target; begin++) {     // 依次遍历小于target的自然数，作为起始数字
-            int sum = 0;
-            for (int j = begin; j < target; j++) {
-                sum += j;
-                if (sum == target) {    // 找到一组解
-                    int[] arr = new int[j - begin + 1];
-                    ans.add(arr);
-                    for (int i = 0; i < j - begin + 1; i++) {
-                        arr[i] = begin + i;
-                    }
+        // 简单但低效的解法：逐个数字作为起点向后延伸
+//        List<int[]> ans = new ArrayList<>();
+//        for (int begin = 1; begin <= target / 2; begin++) {     // 依次遍历小于 target/2 的自然数，作为起始数字
+//            int sum = 0;
+//            for (int j = begin; j < target; j++) {
+//                sum += j;
+//                if (sum == target) {    // 找到一组解
+//                    int[] arr = new int[j - begin + 1];
+//                    ans.add(arr);
+//                    for (int i = 0; i < j - begin + 1; i++) {
+//                        arr[i] = begin + i;
+//                    }
+//
+//                } else if (sum > target) {
+//                    break;  // 退出当前循环
+//                }
+//            }
+//        }
+//        return ans.toArray(new int[0][]);
 
-                } else if (sum > target) {
-                    break;  // 退出当前循环
+        // 滑动窗口：
+        int i = 1; // 滑动窗口的左边界
+        int j = 1; // 滑动窗口的右边界
+        int sum = 0; // 滑动窗口中数字的和
+        List<int[]> res = new ArrayList<>();
+
+        while (i <= target / 2) {
+            if (sum < target) {
+                // 右边界向右移动
+                sum += j;
+                j++;
+            } else if (sum > target) {
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            } else {
+                // 记录结果
+                int[] arr = new int[j-i];
+                for (int k = i; k < j; k++) {
+                    arr[k-i] = k;
                 }
+                res.add(arr);
+                // 左边界向右移动
+                sum -= i;
+                i++;
             }
         }
-        return ans.toArray(new int[0][]);
+
+        return res.toArray(new int[res.size()][]);
     }
 }
 

ead4b6432a709b52cf959cb8736048523376b05f
alstonchen <alstonchen@tencent.com>
2020-12-11 10:42:11 +0800

    add offer 66

--- src/main/java/com/offer/_66_constructArr.java
+++ src/main/java/com/offer/_66_constructArr.java
@@ -16,12 +16,34 @@ package com.offer;
 
 class Solution_66 {
     public int[] constructArr(int[] a) {
-        return null;
+        // 解读：B[i]中的因子不包含A[i],不用考虑溢出
+        // 设想有一个表格，可以分为下三角和上三角，对角线为1（对应A[i]），分别计算两部分的乘积
+        int len = a.length;
+        if (len == 0) return new int[0];   // 空数组
+        int[] b = new int[len];
+        b[0] = 1;
+        // 先计算下三角乘积 从b[1]递推到b[n-1]
+        for (int i = 1; i < len; i++) {
+            b[i] = b[i - 1] * a[i - 1];     // 将前一项乘以a[i-1]即可
+        }
+        // 计算上三角的乘积，从b[n-2]递推到b[0]
+        // 用tmp记录从b[n-1]到b[1]的乘积
+        int tmp = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            tmp *= a[i + 1];
+            b[i] *= tmp;
+        }
+        return b;
     }
 }
 
 public class _66_constructArr {
     public static void main(String[] args) {
         // [1, 2, 3, 4, 5]
+        int[] nums = {1, 2, 3, 4, 5};
+        Solution_66 solution = new Solution_66();
+        int[] ans = solution.constructArr(nums);
+        for (int num : ans)
+            System.out.println(num);
     }
 }

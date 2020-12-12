16b1c247967f588f2df1bea893ab90d540592c9a
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-12 17:18:08 +0800

    update

---
+++ src/main/java/com/code/main/_0_100/TreeNode.java
@@ -0,0 +1,20 @@
+package com.code.main._0_100;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

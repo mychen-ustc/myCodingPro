685c05bf7ef9baf60c484ba7278274f8eefabf04
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-14 08:42:21 +0800

    add code 15

---
+++ src/main/java/com/code/main/_0_100/_0019_removeNthFromEnd.java
@@ -0,0 +1,40 @@
+/**
+ * // 19. 删除链表的倒数第N个节点
+ * // 难度：中等
+ * // 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * //
+ * // 示例：
+ * // 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * //
+ * // 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * //
+ * // 说明：
+ * // 给定的 n 保证是有效的。
+ * //
+ * // 进阶：
+ * // 你能尝试使用一趟扫描实现吗？
+ */
+
+package com.code.main._0_100;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution_0019 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        return null;
+    }
+}
+
+public class _0019_removeNthFromEnd {
+    public static void main(String[] args) {
+        // [1,2,3,4,5] 2
+    }
+}

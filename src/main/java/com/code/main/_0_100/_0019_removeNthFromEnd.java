aee2ae3400ca92fd9d0fb9a2060de91c2a1852cc
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-14 09:27:58 +0800

    add code 19

--- src/main/java/com/code/main/_0_100/_0019_removeNthFromEnd.java
+++ src/main/java/com/code/main/_0_100/_0019_removeNthFromEnd.java
@@ -29,12 +29,47 @@ package com.code.main._0_100;
  */
 class Solution_0019 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+        // 统计链表长度
+        ListNode node = head;
+        int length = 0;    // 链表节点数
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        if (n == length) return head.next;  // 删除第一个节点：直接返回头结点的next
+        // 找到第 length-n 个节点
+        int cnt = 0, k = length - n;
+        node = head;
+        ListNode pre = null;
+        while (cnt < k) {
+            pre = node;     // 记录前驱
+            node = node.next;
+            cnt++;
+        }
+        // 删除节点
+        pre.next = node.next;
+        return head;
     }
 }
 
 public class _0019_removeNthFromEnd {
     public static void main(String[] args) {
         // [1,2,3,4,5] 2
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        ListNode e = new ListNode(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+
+        Solution_0019 solution = new Solution_0019();
+        ListNode ans = solution.removeNthFromEnd(a, 4);
+        while (ans != null) {
+            System.out.println(ans.val);
+            ans = ans.next;
+        }
     }
 }

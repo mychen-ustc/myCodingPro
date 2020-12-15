bc1ea2b52ca9bef8aa4147113bb81571b30c9baa
alstonchen <alstonchen@tencent.com>
2020-12-15 15:52:40 +0800

    add code 79

--- src/main/java/com/code/main/_0_100/_0079_exist.java
+++ src/main/java/com/code/main/_0_100/_0079_exist.java
@@ -28,10 +28,49 @@ package com.code.main._0_100;
 
 class Solution_0079 {
     public boolean exist(char[][] board, String word) {
-        return true;
+        int m = board.length;
+        if (m == 0) return false;
+        int n = board[0].length;
+        boolean[][] visit = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = judge(board, word, 0, i, j, visit);
+                if (flag) return true;  // 已经找到完全匹配的
+            }
+        }
+        return false;
+    }
+
+    public boolean judge(char[][] board, String word, int index, int row, int col, boolean[][] visit) {
+        if (board[row][col] != word.charAt(index)) return false;    // 当前字符不匹配
+        if (index == word.length() - 1) return true;    // 已经找到完全匹配的
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};   // 方向数组
+        visit[row][col] = true;     // 标记当前位置已访问
+        boolean flag = false;
+        for (int i = 0; i < 4; i++) {   // 探索4个方向
+            int newx = row + directions[i][0];
+            int newy = col + directions[i][1];
+            if (newx >= 0 && newx < board.length && newy >= 0 && newy < board[0].length) {
+                if (!visit[newx][newy]) {
+                    flag = judge(board, word, index + 1, newx, newy, visit);
+                    if (flag) {
+                        break;
+                    }
+                }
+            }
+        }
+        return flag;
     }
 }
 
 public class _0079_exist {
-//    [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]] "ABCCED"
+    public static void main(String[] args) {
+        // [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]] "ABCCED"
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'F'}};
+        Solution_0079 solution = new Solution_0079();
+        System.out.println(solution.exist(board, "ABCCED"));
+        System.out.println(solution.exist(board, "ABCCFD"));
+        System.out.println(solution.exist(board, "ABCFD"));
+        System.out.println(solution.exist(board, "ABCESF"));
+    }
 }

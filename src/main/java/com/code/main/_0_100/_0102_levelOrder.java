738d2fb85052d79d5503145a38a8982a30d9fe6f
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-15 08:22:12 +0800

    update

---
+++ src/main/java/com/code/main/_0_100/_0102_levelOrder.java
@@ -0,0 +1,44 @@
+/**
+ * // 102. 二叉树的层序遍历
+ * // 难度：中等
+ * // 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ * //
+ * // 示例：
+ * // 二叉树：[3,9,20,null,null,15,7],
+ * //
+ * //     3
+ * //    / \
+ * //   9  20
+ * //     /  \
+ * //    15   7
+ * // 返回其层次遍历结果：
+ * //
+ * // [
+ * // [3],
+ * // [9,20],
+ * // [15,7]
+ * // ]
+ */
+
+package com.code.main._0_100;
+
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_0102 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}
+
+public class _0102_levelOrder {
+    // [3,9,20,null,null,15,7]
+}

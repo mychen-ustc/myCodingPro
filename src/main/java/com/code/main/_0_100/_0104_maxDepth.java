31abf0834c67422eb621d65c642658f1aa5053a8
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-16 09:11:37 +0800

    add code 104

--- src/main/java/com/code/main/_0_100/_0104_maxDepth.java
+++ src/main/java/com/code/main/_0_100/_0104_maxDepth.java
@@ -36,12 +36,49 @@ package com.code.main._0_100;
  */
 class Solution_0104 {
     public int maxDepth(TreeNode root) {
-        return 0;
+        // 可用DFS或BFS遍历，统计最大层
+        // BFS解法: 非递归
+//        if (root == null) return 0;
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        queue.offer(root);
+//        int maxDepth = 0;
+//        while (!queue.isEmpty()) {
+//            int curLevelCnt = queue.size();
+//            for (int i = 0; i < curLevelCnt; i++) {   // 把队列中，当前层的元素全部处理完
+//                TreeNode node = queue.poll();
+//                if (node.left != null) queue.offer(node.left);
+//                if (node.right != null) queue.offer(node.right);
+//            }
+//            maxDepth++;
+//        }
+//        return maxDepth;
+
+        // DFS解法：递归，实测递归的解法效率更高（100% vs 16%, 0ms vs 1ms）
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
     }
 }
 
 public class _0104_maxDepth {
     public static void main(String[] args) {
         // [3,9,20,null,null,15,7]
+        TreeNode a = new TreeNode(3);
+        TreeNode b = new TreeNode(9);
+        TreeNode c = new TreeNode(20);
+        TreeNode d = new TreeNode(15);
+        TreeNode e = new TreeNode(7);
+        a.left = b;
+        a.right = c;
+        c.left = d;
+        c.right = e;
+
+        Solution_0104 solution = new Solution_0104();
+        int ans = solution.maxDepth(a);
+        System.out.println(ans);
     }
 }

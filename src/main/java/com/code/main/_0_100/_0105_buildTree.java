b90a35138d19d9a1ea29b5c5eb182fd4c19cf8a9
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-16 08:48:45 +0800

    update

---
+++ src/main/java/com/code/main/_0_100/_0105_buildTree.java
@@ -0,0 +1,43 @@
+/**
+ * // 105. 从前序与中序遍历序列构造二叉树
+ * // 难度：中等
+ * // 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * //
+ * // 注意:
+ * // 你可以假设树中没有重复的元素。
+ * //
+ * // 例如，给出
+ * //
+ * // 前序遍历 preorder = [3,9,20,15,7]
+ * // 中序遍历 inorder = [9,3,15,20,7]
+ * // 返回如下的二叉树：
+ * //
+ * //    3
+ * //   / \
+ * //  9  20
+ * //    /  \
+ * //   15   7
+ */
+
+package com.code.main._0_100;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution_0105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return null;
+    }
+}
+
+public class _0105_buildTree {
+    public static void main(String[] args) {
+        // [3,9,20,15,7] [9,3,15,20,7]
+    }
+}

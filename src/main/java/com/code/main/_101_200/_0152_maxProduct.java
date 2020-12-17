c929149ce830c0be5ab77c1d8ce473c818e97eaf
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-17 09:33:15 +0800

    add code 152

--- src/main/java/com/code/main/_101_200/_0152_maxProduct.java
+++ src/main/java/com/code/main/_101_200/_0152_maxProduct.java
@@ -18,12 +18,31 @@ package com.code.main._101_200;
 
 class Solution_0152 {
     public int maxProduct(int[] nums) {
-        return 0;
+        // 动态规划：由于数字可能为正可能有负，需要记录前面的最大和最小值
+        // dpMax[i]为以当前元素为最后一个元素的子数组的最大乘积,dpMIn[i]为以当前元素为最后一个元素的子数组的最小乘积
+        int len = nums.length;
+        if (len == 0) return 0;
+        if (len == 1) return nums[0];
+        int[] dpMax = new int[len];     // 维护最大乘积
+        int[] dpMin = new int[len];     // 维护最小乘积
+        System.arraycopy(nums, 0, dpMax, 0, len);   // 初始化
+        System.arraycopy(nums, 0, dpMin, 0, len);   // 初始化
+        int maxProduct = dpMax[0];
+        for (int i = 1; i < len; i++) {
+            dpMax[i] = Math.max(dpMax[i - 1] * nums[i], Math.max(nums[i], dpMin[i - 1] * nums[i])); // 正的最大值
+            dpMin[i] = Math.min(dpMin[i - 1] * nums[i], Math.min(nums[i], dpMax[i - 1] * nums[i])); // 负的最小值
+            maxProduct = Math.max(maxProduct, dpMax[i]);
+        }
+        return maxProduct;
     }
 }
 
 public class _0152_maxProduct {
     public static void main(String[] args) {
         // [2,3,-2,4]
+        int[] nums = {2, 3, -2, 4};
+        Solution_0152 solution = new Solution_0152();
+        int ans = solution.maxProduct(nums);
+        System.out.println(ans);
     }
 }

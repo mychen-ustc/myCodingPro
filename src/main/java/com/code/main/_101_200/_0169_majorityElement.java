892656226a7186f303c47c4007a63169f6248144
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-17 08:31:54 +0800

    update

---
+++ src/main/java/com/code/main/_101_200/_0169_majorityElement.java
@@ -0,0 +1,29 @@
+/**
+ * // 169. 多数元素
+ * // 难度：简单
+ * // 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * //
+ * // 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * //
+ * // 示例 1:
+ * // 输入: [3,2,3]
+ * // 输出: 3
+ * //
+ * // 示例 2:
+ * // 输入: [2,2,1,1,1,2,2]
+ * // 输出: 2
+ */
+
+package com.code.main._101_200;
+
+class Solution_0169 {
+    public int majorityElement(int[] nums) {
+        return 0;
+    }
+}
+
+public class _0169_majorityElement {
+    public static void main(String[] args) {
+        // [3,2,3]
+    }
+}

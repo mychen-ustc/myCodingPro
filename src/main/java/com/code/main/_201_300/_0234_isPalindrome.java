e9421289f7151fad9f798ac5ed49d67ff291770a
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-18 09:11:23 +0800

    update

---
+++ src/main/java/com/code/main/_201_300/_0234_isPalindrome.java
@@ -0,0 +1,38 @@
+/**
+ * 234. 回文链表
+ * 难度：简单
+ * 请判断一个链表是否为回文链表。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1->2
+ * 输出: false
+ * 示例 2:
+ * <p>
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ */
+
+package com.code.main._201_300;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution_0234 {
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
+
+public class _0234_isPalindrome {
+    public static void main(String[] args) {
+        // [1,2]
+    }
+}
---
+++ src/main/java/com/code/main/_201_300/_0283_moveZeroes.java
@@ -0,0 +1,28 @@
+/**
+ * // 283. 移动零
+ * // 难度：中等
+ * // 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * //
+ * // 示例:
+ * // 输入: [0,1,0,3,12]
+ * // 输出: [1,3,12,0,0]
+ * //
+ * // 说明:
+ * //
+ * // 必须在原数组上操作，不能拷贝额外的数组。
+ * // 尽量减少操作次数。
+ */
+
+package com.code.main._201_300;
+
+class Solution_0283 {
+    public void moveZeroes(int[] nums) {
+
+    }
+}
+
+public class _0283_moveZeroes {
+    public static void main(String[] args) {
+        // [0,1,0,3,12]
+    }
+}

6ab7cc0638bfb96ccbbe5993ab1fab6d69c7795a
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-19 10:27:54 +0800

    add code 279

--- src/main/java/com/code/main/_201_300/_0279_numSquares.java
+++ src/main/java/com/code/main/_201_300/_0279_numSquares.java
@@ -16,14 +16,30 @@
 
 package com.code.main._201_300;
 
+import java.util.Arrays;
+
 class Solution_0279 {
     public int numSquares(int n) {
-        return 0;
+        // 思路1: 动态规划，递推计算从1~n的最小平方数个数
+        // 有更优的数学解法，但是不容易想，做到动态规划就可以了
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);     // 初始化数组
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int sqrt = (int) Math.sqrt(i);  // 开根号
+            for (int j = 1; j <= sqrt; j++) {   // 遍历不超过sqrt的平方数进行
+                int idx = i - j * j;
+                dp[i] = Math.min(dp[i], dp[idx] + 1);
+            }
+        }
+        return dp[n];
     }
 }
 
 public class _0279_numSquares {
     public static void main(String[] args) {
-
+        Solution_0279 solution = new Solution_0279();
+        int ans = solution.numSquares(12);
+        System.out.println(ans);
     }
 }

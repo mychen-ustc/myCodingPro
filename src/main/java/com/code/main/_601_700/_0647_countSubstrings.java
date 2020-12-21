ed28a02833937e2ae44dfe10715f0568c625080d
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2020-12-21 22:51:48 +0800

    add code 0647

--- src/main/java/com/code/main/_601_700/_0647_countSubstrings.java
+++ src/main/java/com/code/main/_601_700/_0647_countSubstrings.java
@@ -23,7 +23,7 @@ package com.code.main._601_700;
 
 class Solution_0647 {
     public int countSubstrings(String s) {
-        // 解法1: 朴素做法，穷举所有的子串，检查是否为回文串 时间O(n^3)
+        // 解法1: 朴素做法，穷举所有的子串，检查是否为回文串 时间O(n^3) 5%
 //        int ans = 0;
 //        int len = s.length();
 //        for (int i = 0; i < len; i++) {
@@ -35,48 +35,49 @@ class Solution_0647 {
 //        }
 //        return ans;
 
-        // 解法2: 中心扩散 O(n^2) & O(1)
-        int n = s.length(), ans = 0;
-        for (int i = 0; i < 2 * n - 1; i++) {
-            int left = i / 2, right = i / 2 + i % 2;
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                left--;
-                right++;
-                ans++;
-            }
-        }
-        return ans;
-
-        // 解法3: Manacher算法 O(n) & O(n)
-        // 每2个字符之间添加特殊符号将长度扩充到2n+1
-//        int n = s.length();
-//        StringBuffer t = new StringBuffer("$#");
-//        for (int i = 0; i < n; ++i) {
-//            t.append(s.charAt(i));
-//            t.append('#');
-//        }
-//        n = t.length();
-//        t.append('!');
-//
-//        int[] f = new int[n];
-//        int iMax = 0, rMax = 0, ans = 0;
-//        for (int i = 1; i < n; ++i) {
-//            // 初始化 f[i]
-//            f[i] = i <= rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;
-//            // 中心拓展
-//            while (t.charAt(i + f[i]) == t.charAt(i - f[i])) {
-//                ++f[i];
+        // 解法2: 中心扩散 O(n^2) & O(1) 80%
+//        int n = s.length(), ans = 0;
+//        for (int i = 0; i < 2 * n - 1; i++) {
+//            int left = i / 2, right = i / 2 + i % 2;
+//            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+//                left--;
+//                right++;
+//                ans++;
 //            }
-//            // 动态维护 iMax 和 rMax
-//            if (i + f[i] - 1 > rMax) {
-//                iMax = i;
-//                rMax = i + f[i] - 1;
-//            }
-//            // 统计答案, 当前贡献为 (f[i] - 1) / 2 上取整
-//            ans += f[i] / 2;
 //        }
-//
 //        return ans;
+
+        // 解法3: Manacher算法 O(n) & O(n)  92.5%
+        // 每2个字符之间添加特殊符号将长度扩充到2n+1, 在开头和结尾多加个$和!，保证循环不会越界（正常终止）
+        // 初始化的公司稍微难理解
+        int n = s.length();
+        StringBuffer buffer = new StringBuffer("$#");
+        for (int i = 0; i < n; ++i) {
+            buffer.append(s.charAt(i));
+            buffer.append('#');
+        }
+        n = buffer.length();
+        buffer.append('!');
+
+        int[] f = new int[n];
+        int iMax = 0, rMax = 0, ans = 0;    // 用rMax记录历史右端点的最大的回文串的右端点，iMax指向对应的中心
+        for (int i = 1; i < n; ++i) {
+            // 初始化 f[i] 有两个限制: 1.初始值不超过右端点rmax，2.对称点的半径
+            f[i] = i <= rMax ? Math.min(rMax - i + 1, f[2 * iMax - i]) : 1;     // 处理每个位置时，先用已经算出来的结果初始化
+            // 中心拓展
+            while (buffer.charAt(i + f[i]) == buffer.charAt(i - f[i])) {    // 一直扩展到两个端点不相同
+                ++f[i];
+            }
+            // 动态维护 iMax 和 rMax
+            if (i + f[i] - 1 > rMax) {
+                iMax = i;
+                rMax = i + f[i] - 1;
+            }
+            // 统计答案, 当前贡献为 (f[i] - 1) / 2 上取整
+            ans += f[i] / 2;    // 字母一定是在中心扩展的偶数位上（奇数位为#）
+        }
+
+        return ans;
     }
 
     // 判断是不是回文串
@@ -96,9 +97,10 @@ public class _0647_countSubstrings {
     public static void main(String[] args) {
         // aaabcbcba
         Solution_0647 solution = new Solution_0647();
-        System.out.println(solution.check("bcb"));
-        System.out.println(solution.countSubstrings("abc"));
-        System.out.println(solution.countSubstrings("aaa"));
-        System.out.println(solution.countSubstrings("aaabcbcba"));
+//        System.out.println(solution.check("bcb"));
+//        System.out.println(solution.countSubstrings("abc"));
+//        System.out.println(solution.countSubstrings("aaa"));
+//        System.out.println(solution.countSubstrings("aaabcbcba"));
+        System.out.println(solution.countSubstrings("abacabac"));
     }
 }

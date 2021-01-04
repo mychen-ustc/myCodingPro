04029908c0f842d81e74a7456d333e08b73ac1ca
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2021-01-04 09:06:58 +0800

    add code 9

--- src/main/java/com/code/main/_0_100/_0009_isPalindrome.java
+++ src/main/java/com/code/main/_0_100/_0009_isPalindrome.java
@@ -25,12 +25,49 @@ package com.code.main._0_100;
 
 class Solution_0009 {
     public boolean isPalindrome(int x) {
-        return true;
+        // 将数字转字符串很容易进行遍历
+        // 如果要求不能将数字转字符串，可以将数字反转，比较与原来的数字是否相等。要注意溢出问题
+//        if (x < 0) return false;    // 负数反转不是合法数字
+//        long num = Math.abs(x);
+//        long rev = 0;   // 反转之后的数字
+//        long tmp = num;
+//        while (tmp > 0) {
+//            rev = rev * 10 + tmp % 10;
+//            if (rev > Integer.MAX_VALUE)
+//                return false;
+//            tmp /= 10;
+//        }
+//        return num == rev;
+
+
+        // 优化解法：只反转一半数字 (9ms vs 11ms, 99% vs 49%)
+        // 特殊情况：
+        // 如上所述，当 x < 0 时，x 不是回文数。
+        // 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+        // 则其第一位数字也应该是 0
+        // 只有 0 满足这一属性
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        // 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+        // 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+        // 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
 
 public class _0009_isPalindrome {
     public static void main(String[] args) {
         // 121
+        Solution_0009 solution = new Solution_0009();
+        System.out.println(solution.isPalindrome(121));
+        System.out.println(solution.isPalindrome(-121));
+        System.out.println(solution.isPalindrome(10));
+
     }
 }

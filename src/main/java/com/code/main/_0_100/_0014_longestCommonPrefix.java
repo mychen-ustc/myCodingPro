236d2c82eea6ee1a4fbe2c52febcfe65267e90b2
mychen <wx_6a56e7e8e7ec4720b9b89007272a618d@git.code.tencent.com>
2021-01-04 09:16:08 +0800

    update

---
+++ src/main/java/com/code/main/_0_100/_0014_longestCommonPrefix.java
@@ -0,0 +1,33 @@
+/**
+ * // 14. 最长公共前缀
+ * // 难度：简单
+ * // 编写一个函数来查找字符串数组中的最长公共前缀。
+ * //
+ * // 如果不存在公共前缀，返回空字符串 ""。
+ * //
+ * // 示例 1:
+ * // 输入: ["flower","flow","flight"]
+ * // 输出: "fl"
+ * //
+ * // 示例 2:
+ * // 输入: ["dog","racecar","car"]
+ * // 输出: ""
+ * // 解释: 输入不存在公共前缀。
+ * //
+ * // 说明:
+ * // 所有输入只包含小写字母 a-z 。
+ */
+
+package com.code.main._0_100;
+
+class Solution_0014 {
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+}
+
+public class _0014_longestCommonPrefix {
+    public static void main(String[] args) {
+        // ["flower","flow","flight"]
+    }
+}
---
+++ src/main/java/com/code/main/_0_100/_0016_threeSumClosest.java
@@ -0,0 +1,30 @@
+/**
+ * // 16. 最接近的三数之和
+ * // 难度：中等
+ * // 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * //
+ * // 示例：
+ * // 输入：nums = [-1,2,1,-4], target = 1
+ * // 输出：2
+ * // 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ * //  
+ * //
+ * // 提示：
+ * // 3 <= nums.length <= 10^3
+ * // -10^3 <= nums[i] <= 10^3
+ * // -10^4 <= target <= 10^4
+ */
+
+package com.code.main._0_100;
+
+class Solution_0016 {
+    public int threeSumClosest(int[] nums, int target) {
+        return 0;
+    }
+}
+
+public class _0016_threeSumClosest {
+    public static void main(String[] args) {
+        // [-1,2,1,-4] 1
+    }
+}
